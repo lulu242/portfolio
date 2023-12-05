b832377ff7ba8218071d8d930022a0e8d014338b
lulu242 <119933024+lulu242@users.noreply.github.com>
2023-12-05 16:48:54 +0900

    style: css 색상 변수 추가

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -8,7 +8,7 @@ const Title:FC<titleProps> = ({ title }) => {
   return (
     <span className='relative p-2'>
       <span className='relative left-[-10px] z-10 font-black pr-4'>{title}</span>
-      <span className='h-full absolute left-0 top-0 translate-x-[-15px] skew-x-[-20deg] w-full overflow-hidden before:bg-[#EFFF8C] before:h-full before:absolute before:w-full inset-0' ></span>
+      <span className='h-full absolute left-0 top-0 translate-x-[-15px] skew-x-[-20deg] w-full overflow-hidden before:highlighted before:h-full before:absolute before:w-full inset-0' ></span>
     </span>
   )
 }
