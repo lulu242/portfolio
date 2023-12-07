18447bd0409112c8e1539b61214c8f502a5cf720
lulu242 <119933024+lulu242@users.noreply.github.com>
2023-12-07 11:22:40 +0900

    refactor: 중복되는 부분 통합

--- src/components/Nav/NavList.tsx
+++ src/components/Nav/NavList.tsx
@@ -3,50 +3,37 @@ import { Link } from 'react-scroll';
 
 interface NavListProps {
   isNavActive: boolean;
-  setIsNavActive: React.Dispatch<React.SetStateAction<boolean>>
+  setIsNavActive: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const NavList :FC<NavListProps> = React.memo(({isNavActive, setIsNavActive}) => {
-  return (
-    <>
-      <li className="cursor-pointer hover:text-gray-600">
-        <Link
-          onClick={() => setIsNavActive(!isNavActive)}
-          activeClass="underline decoration-solid"
-          spy={true}
-          to="aboutMe"
-          smooth={true}
-          duration={500}
-        >
-          ABOUT ME
-        </Link>
-      </li>
-      <li className="cursor-pointer hover:text-gray-600">
-        <Link
-          onClick={() => setIsNavActive(!isNavActive)}
-          activeClass="underline decoration-solid"
-          spy={true}
-          to="skills"
-          smooth={true}
-          duration={500}
-        >
-          SKILLS
-        </Link>
-      </li>
-      <li className="cursor-pointer hover:text-gray-600">
-        <Link
-          onClick={() => setIsNavActive(!isNavActive)}
-          activeClass="underline decoration-solid"
-          spy={true}
-          to="project"
-          smooth={true}
-          duration={500}
-        >
-          PROJECT
-        </Link>
-      </li>
-    </>
-  );
-});
+const NavList: FC<NavListProps> = React.memo(
+  ({ isNavActive, setIsNavActive }) => {
+    const navLists = [
+      { to: 'aboutMe', value: 'ABOUT ME' },
+      { to: 'skills', value: 'SKILLS' },
+      { to: 'project', value: 'PROJECT' },
+    ];
+
+    return (
+      <>
+        {navLists.map(({to, value}) => (
+          <li className="cursor-pointer hover:text-gray-600">
+            <Link
+              onClick={() => setIsNavActive(!isNavActive)}
+              activeClass="underline decoration-solid"
+              spy={true}
+              to={to}
+              smooth={true}
+              duration={500}
+            >
+              {value}
+            </Link>
+          </li>
+        ))}
+        
+      </>
+    );
+  }
+);
 
 export default NavList;
