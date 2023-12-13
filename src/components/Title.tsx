9beb05cc34cdef523b0a0da5f25153c1b50c461f
lulu242 <119933024+lulu242@users.noreply.github.com>
2023-12-13 22:54:12 +0900

    design: Title padding 수정

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -7,8 +7,8 @@ interface titleProps {
 
 const Title:FC<titleProps> = ({ title, isTransition = false }) => {
   return (
-    <span className='relative inline-block px-2 py-1 overflow-y-clip'>
-      <span className={`relative left-4 z-10 font-black pr-4 inline-block ${isTransition && 'translate-y-full'}`}>{title}</span>
+    <span className='relative inline-block px-5 py-2 overflow-y-clip'>
+      <span className={`relative left-4 z-10 font-black text-4xl pr-4 inline-block w-auto ${isTransition && 'translate-y-[110%]'}`}>{title}</span>
       <span className={`h-full absolute left-6 top-0 skew-x-[-20deg] before:highlighted before:h-full before:absolute before:w-full translate-x-[-15px] ${isTransition ? 'w-0' : 'w-full'} inset-0`} ></span>
     </span>
   )
