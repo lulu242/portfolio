997c56683dfb58689c250bd07b22a0cb04b9c3b1
lulu242 <119933024+lulu242@users.noreply.github.com>
2023-12-13 22:54:55 +0900

    feat: About page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useMemo } from 'react';
+import React, { FC } from 'react';
 import Title from '../components/Title';
 import profile from '../image/profile.png';
 import useScrollObserver from '../hook/useScrollObserver';
@@ -18,40 +18,62 @@ const hrefLists = [
 
 export const About: FC = () => {
   const [targetRef, isVisible] = useScrollObserver();
-  const HrefLists = 
-      hrefLists.map(({ href, title }, idx) => (
-        <a id={title} key={title} href={href} target="_blank" rel="noreferrer" ref={(el) => {if(el !== null) {targetRef.current[idx] = el}}} className={isVisible[title] ? 'show' : ''}>
-          <Title title={title} isTransition={true}/>
-        </a>
-      ))
-    
-  
-  console.log(isVisible)
+  const HrefLists = hrefLists.map(({ href, title }, idx) => (
+    <div>
+      <a
+        id={title}
+        key={title}
+        href={href}
+        target="_blank"
+        rel="noreferrer"
+        ref={(el) => {
+          if (el !== null) {
+            targetRef.current[idx] = el;
+          }
+        }}
+        className={`${isVisible[title] ? 'show' : ''}`}
+      >
+        <Title title={title} isTransition={true} />
+      </a>
+    </div>
+  ));
+
   return (
-    <section id="aboutMe" className="h-screen">
-      <h1 className='visuallyhidden'>자기소개</h1>
-      <div className="max-w-4xl mx-auto">
-        <div className="flex justify-around py-40">
-          <div className="bg-gradient-to-r from-[#FAACA8] to-[#DDD6F3] rounded-full w-80 h-80 overflow-hidden">
+    <section id="aboutMe" className="h-screen min-h-[800px]">
+      <h1 className="visuallyhidden">자기소개</h1>
+      <div className="flex flex-col justify-center h-full max-w-[1240px] m-auto overflow-hidden">
+        <div className="flex justify-around pb-20">
+          <div className="bg-gradient-to-r from-[#FAACA8] to-[#DDD6F3] rounded-full decoration-4 h-full overflow-hidden">
             <img src={profile} alt="profile" className="h-full"></img>
           </div>
           <div className="flex flex-col justify-around">{HrefLists}</div>
         </div>
-        <div className=''>
-          <p>React를 중심으로 웹 개발이 가능한 프론트엔드 개발자입니다.</p>
-          <p>
-            매일 사용하는 웹 사이트를 만들어 볼 수 있다는 점에 개발자에 흥미를
-            느끼게 되었고, 화면을 직접 구현한다는 점에서 프론트엔드에 관심을
-            두게 되었습니다. 여러 사람이 봤을 때도 매력적인 웹 서비스를 실제로
-            만든다면 굉장한 성취감을 느낄 것 같아 개발자에 도전하게 되었습니다.
-            그 후 빠른 학습과 실전 프로젝트 경험을 위해 코드스테이츠에서 6개월
-            동안 프론트엔드 부트캠프에 참여하였습니다. 부트캠프를 진행하면서
-            학습한 내용을 블로그에 요약했었습니다.{' '}
+        <div
+          className={`text-xl leading-10 ${
+            isVisible.aboutText ? 'translate-y-0' : 'translate-y-1/2'
+          } `}
+          ref={(el) => {
+            if (el !== null) {
+              targetRef.current.push(el);
+            }
+          }}
+          id="aboutText"
+        >
+          <p className="text-2xl font-semibold">
+            안녕하세요, 프론트엔드 주니어 개발자 원영은입니다!
           </p>
           <p>
-            부트캠프에서 빠르게 많은 내용을 학습하다 보니 깊게는 학습하지는
-            못해서 현재는 놓친 부분을 채우고, 배우지 못했던 typeScript와 Next.js
-            학습하여 더 나은 개발자로 성장하고자 합니다.
+            많은 사람이 매력을 느낄 수 있는 웹 서비스를 구현하고,{' '}
+            <span>
+              배운 기술을 적용하며 직접 구현해가는 과정이 저에게는 큰 즐거움
+            </span>
+            입니다. 일상에서 마주치는 불편함에 대한 생각해 보며,{' '}
+            <span>더 나은 사용자 경험</span>에 대해 고민합니다.
+          </p>
+          <p>
+            <span>끊임없는 도전과 학습</span>을 통해 발전해가는 개발자로
+            성장하고자 합니다. 지속적인 열정과 노력으로 목표를 달성하는데
+            기여하고, 함께 성장할 수 있는 기회를 찾고 있습니다. 감사합니다!
           </p>
         </div>
       </div>
