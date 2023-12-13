616f20bb47856181747ab81f76a2f5e3aeb7f859
lulu242 <119933024+lulu242@users.noreply.github.com>
2023-12-13 22:50:16 +0900

    remove: Title App에서 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css';
-import Nav from './components/Nav';
+import Nav from './components/Nav/Nav';
 import Title from './components/Title';
 import { About } from './pages/About';
 import Hero from './pages/Hero';
 import { Porject } from './pages/Project';
+import ProjectDetail from './components/ProjectDetail';
 import Skills from './pages/Skills';
 
 function App() {
@@ -14,7 +15,6 @@ function App() {
       <About />
       <Skills />
       <Porject />
-      <Title title="GitHub" />
     </div>
   );
 }
